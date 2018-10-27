1e1879961e10ff5c345bf56a5821f00a1a1148c2
uncompiled <chris@uncompiled.org>
2018-10-27 13:02:12 -0400

    Update serverless.yml service definition

--- server/handler.js
+++ server/handler.js
@@ -1,14 +1,13 @@
 'use strict';
 
-module.exports.reportObstruction = async (event, context) => {
+module.exports.report = async (event, context) => {
   return {
     statusCode: 200,
     body: JSON.stringify({
       message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
+      event,
+      context
     }),
   };
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
